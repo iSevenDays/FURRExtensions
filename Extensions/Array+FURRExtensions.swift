80649356945fb9b32d8e7b7303354534d7978fbf
Ruotger Deecke <roddi@me.com>
2015-10-12 10:58:47 +0200

    initial commit of the actual code

---
+++ Extensions/Array+FURRExtensions.swift
@@ -0,0 +1,80 @@
+//
+//  Array+FURRExtensions.swift
+//  DBDB
+//
+//  Created by Ruotger Deecke on 14.01.15.
+//  Copyright (c) 2015 Deecke,Roddi. All rights reserved.
+//
+// TL/DR; BSD 2-clause license
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+// following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
+//    disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+//    following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import Foundation
+
+public extension Array {
+
+    public func reversed() -> Array<Element> {
+        var result:[Element] = Array()
+        var mutSelf:[Element] = self
+        while mutSelf.count != 0 {
+            if let last = mutSelf.last {
+                result.append(last)
+                mutSelf.removeLast()
+            }
+        }
+        return result
+    }
+
+    public func optionalElementAtIndex(inIndex:Int) -> Element? {
+        if inIndex < 0 {
+            return nil
+        }
+
+        if inIndex >= self.count {
+            return nil
+        }
+
+        return self[inIndex];
+    }
+
+    public func divideUntil(divisionBlock: (inElement:Element) -> Bool) -> (Array<Element>,Array<Element>) {
+        var beforeArray:Array<Element> = Array()
+        var afterArray:Array<Element> = Array()
+
+        var hasFound = false
+        for element in self {
+            if hasFound {
+                afterArray.append(element)
+                continue;
+            }
+
+            hasFound = divisionBlock(inElement: element)
+
+            if !hasFound {
+                beforeArray.append(element)
+            }
+        }
+
+        return (beforeArray,afterArray)
+    }
+
+}
+
+
+
---
+++ ExtensionsTests/FURRExtensionTests.swift
@@ -0,0 +1,95 @@
+//
+//  ExtensionTests.swift
+//  DBDB
+//
+//  Created by Ruotger Deecke on 21.08.15.
+//  Copyright © 2015 Deecke,Roddi. All rights reserved.
+//
+// TL/DR; BSD 2-clause license
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+// following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+//    disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+//    following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import XCTest
+
+class ExtensionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testArrayReverse() {
+        let array = ["a","b","c","d"]
+        let reversed = array.reversed()
+        XCTAssert(reversed == ["d","c","b","a"])
+    }
+
+    func testOptionalElement() {
+        let array = ["a","b","c","d"]
+        XCTAssert(array.optionalElementAtIndex(0) != nil)
+        XCTAssert(array.optionalElementAtIndex(-1) == nil)
+        XCTAssert(array.optionalElementAtIndex(3) != nil)
+        XCTAssert(array.optionalElementAtIndex(4) == nil)
+    }
+
+    func testDivideUntilMiddle() {
+        let array = ["a","b","c","d","e"]
+        let (before,after) = array.divideUntil { (inElement) -> Bool in
+            inElement == "c"
+        }
+
+        XCTAssert(before == ["a","b"])
+        XCTAssert(after == ["d","e"])
+    }
+
+    func testDivideUntilFirst() {
+        let array = ["a","b","c","d","e"]
+        let (before,after) = array.divideUntil { (inElement) -> Bool in
+            inElement == "a"
+        }
+
+        XCTAssert(before == [])
+        XCTAssert(after == ["b","c","d","e"])
+    }
+
+    func testDivideUntilLast() {
+        let array = ["a","b","c","d","e"]
+        let (before,after) = array.divideUntil { (inElement) -> Bool in
+            inElement == "e"
+        }
+
+        XCTAssert(before == ["a","b","c","d"])
+        XCTAssert(after == [])
+    }
+
+    func testDivideUntilWithout() {
+        let array = ["a","b","c","d","e"]
+        let (before,after) = array.divideUntil { (inElement) -> Bool in
+            inElement == "f"
+        }
+
+        XCTAssert(before == ["a","b","c","d","e"])
+        XCTAssert(after == [])
+    }
+    
+}
