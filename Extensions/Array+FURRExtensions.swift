3c4da46b658c37da7320da2535402699dd972584
Ruotger Deecke <roddi@me.com>
2016-02-15 17:06:40 +0100

    updated copyright notice

--- Extensions/Array+FURRExtensions.swift
+++ Extensions/Array+FURRExtensions.swift
@@ -4,7 +4,7 @@
 //  DBDB
 //
 //  Created by Ruotger Deecke on 14.01.15.
-//  Copyright (c) 2015 Deecke,Roddi. All rights reserved.
+//  Copyright (c) 2015-2016 Deecke,Roddi. All rights reserved.
 //
 // TL/DR; BSD 2-clause license
 //
